e917260a69bdc06708e6fbe572adc54db7568861
Emanuil Karapachov <406743@student.fontys.nl>
2023-01-01 15:07:47 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,5 @@ export * from './events/base-publisher';
 export * from './events/item-created-event';
 export * from './events/item-updated-event';
 export * from './events/subjects';
+
+export * from './events/types/order-status';
